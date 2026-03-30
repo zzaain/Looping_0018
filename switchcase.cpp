ec8b9a0d9582fbbea0a3d719ee704895a60bd724
zzaain <sheriellaghzain@gmail.com>
2026-03-30 14:04:53 +0700

    menambahkan perulangan FOR

--- switchcase.cpp
+++ switchcase.cpp
@@ -4,3 +4,11 @@ using namespace std;
 
 int main(){
     int x;
+    
+    //Perulangan dengan FOR
+    //mencetak tulisan seabnyak 5 kali
+    cout<<"PERULANGAN FOR"<<endl;
+    for(int i=0;i<=4;i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+    }
+    cout<<endl;
