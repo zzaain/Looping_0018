0a122943903e2a89f92171dd49df1e8c54f395b4
zzaain <sheriellaghzain@gmail.com>
2026-03-30 13:58:20 +0700

    membuat prosedur input

--- looping.cpp
+++ looping.cpp
@@ -5,3 +5,14 @@ using namespace std;
 int kodeJurusan;
 string namaJurusan;
 string statusKuota;
+
+// prosedur input
+void input() {
+    cout << "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
+    cout << "1. Teknologi Informasi" << endl;
+    cout << "2. Teknik Elektro" << endl;
+    cout << "3. Sistem Informasi" << endl;
+    cout << "----------------------------------" << endl;
+    cout << "Masukkan kode jurusan (1-3): ";
+    cin >> kodeJurusan;
+}
