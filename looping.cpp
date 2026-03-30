be0aadcb747e2d1c76cbe8a50d214d170d75a459
zzaain <sheriellaghzain@gmail.com>
2026-03-30 14:00:28 +0700

    menambahkan output dan int main

--- looping.cpp
+++ looping.cpp
@@ -37,3 +37,19 @@ void pilihanJurusan() {
             statusKuota = "Error: Kode Tidak Valid";
     }
 }
+
+// prosedur output
+void output() {
+    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "Jurusan Pilihan : " << namaJurusan << endl;
+    cout << "Status Kuota    : " << statusKuota << endl;
+    cout << "----------------------------------" << endl;
+}
+
+int main() {
+    input();
+    pilihanJurusan();
+    output();
+
+    return 0;
+}
