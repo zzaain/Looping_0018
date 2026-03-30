b4b05b99db57324071ea12d6a6ac5b37d3dea0e1
zzaain <sheriellaghzain@gmail.com>
2026-03-30 14:09:01 +0700

    menambahkan DO WHILE + penutup program pertama

--- switchcase.cpp
+++ switchcase.cpp
@@ -27,3 +27,15 @@ int main(){
 
     cout<<"Bilangan acak while yang terakhir = "<<x<<endl;
     cout<<endl;
+
+     //Perulangan dengan DO...WHILE
+    cout<<"PERULANGAN DO...WHILE"<<endl;
+    x = 1 + rand()%10;
+    do{
+        cout<<"Bilangan acak = "<<x<<endl;
+        x = rand()%10;
+    }while(x<=5);
+
+    cout<<"Bilangan acak do-while yang terakhir = "<<x<<endl;
+    cout<<endl;
+}
