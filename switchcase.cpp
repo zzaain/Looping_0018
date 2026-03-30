613003a162f9154ec89f7a05d537c62e6a1cda2a
zzaain <sheriellaghzain@gmail.com>
2026-03-30 14:07:54 +0700

    menambahkan random + WHILE

--- switchcase.cpp
+++ switchcase.cpp
@@ -12,3 +12,18 @@ int main(){
         cout<<"Teknologi Informasi UMY"<<endl;
     }
     cout<<endl;
+
+    //untuk mendapatkan urutan angka acak yg berbeda
+    srand(time(0));
+
+    //Perulangan dengan WHILE
+    cout<<"PERULANGAN WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    while(x<=5){
+        cout<<"Bilangan acak = "<<x<<endl;
+        x = rand()%10;
+    }
+
+    cout<<"Bilangan acak while yang terakhir = "<<x<<endl;
+    cout<<endl;
